b4c9ed1ea53eaa8fa7d5e94b9de1ac86d201de26
Lauriche <lauriche.tsongouang-songfack@etu.univ-orleans.fr>
2023-12-26 20:59:37 +0100

    Page d'accueil front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CardComponent } from './component/card/card.component';
+import { HomeComponent } from './component/home/home.component';
+import { CardService } from './component/card/service/card.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    CardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
+  providers: [CardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
