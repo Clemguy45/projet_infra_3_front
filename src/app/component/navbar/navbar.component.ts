f748f6f47b1a9c647c7f68daa815b83eb8eb5eed
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-01-05 16:45:16 +0100

    amélioration de la navbar

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  handleSignUpClick() {
+    // Logique pour gérer le clic sur le bouton d'inscription
+    console.log('Sign Up button clicked');
+}
 
+handleLoginClick() {
+    // Logique pour gérer le clic sur le bouton de connexion
+    console.log('Login button clicked');
+}
 }
