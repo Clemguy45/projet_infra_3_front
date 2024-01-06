2c40e4268cf9d0e43968c246308bc27d887f95a8
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-01-06 02:40:57 +0100

    test mise en place de l'url azure

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://projet-infra-3-backend-dev.azurewebsites.net/'
+    apiUrl: 'https://projet-infra-3-backend-dev.azurewebsites.net'
   };
   
   /*
